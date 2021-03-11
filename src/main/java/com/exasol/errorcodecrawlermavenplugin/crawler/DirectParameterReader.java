4b248afb67d45289f1de49794534ba6ae622c392
Jakob Braun <jakob.braun@posteo.de>
2021-03-11 07:13:38 -0400

    changes from review

--- src/main/java/com/exasol/errorcodecrawlermavenplugin/crawler/DirectParameterReader.java
+++ src/main/java/com/exasol/errorcodecrawlermavenplugin/crawler/DirectParameterReader.java
@@ -12,17 +12,17 @@ class DirectParameterReader {
     /**
      * Read the inline parameters from messages.
      * 
-     * @param numInlineParameterArguments count of arguments used for inline parameters
-     * @param text                        text containing the placeholders
-     * @param errorCodeBuilder            {@link ErrorMessageDeclaration.Builder} to append the parameters to
+     * @param inlineParameterArgumentsCount count of arguments used for inline parameters
+     * @param text                          text containing the placeholders
+     * @param errorCodeBuilder              {@link ErrorMessageDeclaration.Builder} to append the parameters to
      */
-    public void readInlineParameters(final int numInlineParameterArguments, final String text,
+    public void readInlineParameters(final int inlineParameterArgumentsCount, final String text,
             final ErrorMessageDeclaration.Builder errorCodeBuilder) {
-        if (numInlineParameterArguments > 0) {
+        if (inlineParameterArgumentsCount > 0) {
             final Iterable<Placeholder> placeholders = PlaceholderMatcher.findPlaceholders(text);
             int placeholderCounter = 0;
             for (final Placeholder placeholder : placeholders) {
-                if (placeholderCounter >= numInlineParameterArguments) {
+                if (placeholderCounter >= inlineParameterArgumentsCount) {
                     break; // there is not argument for this placeholder
                 }
                 errorCodeBuilder.addParameter(placeholder.getName(), null, !placeholder.isUnquoted());
