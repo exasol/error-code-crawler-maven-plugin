fc689b767090eb6729e3f2fafcd77f27c6f5fedb
Christoph Pirkl <christoph.pirkl@exasol.com>
2022-02-14 11:53:26 +0100

    Add tests for identifier format validation

--- src/test/java/com/exasol/errorcodecrawlermavenplugin/validation/ErrorIdentifierValidatorTest.java
+++ src/test/java/com/exasol/errorcodecrawlermavenplugin/validation/ErrorIdentifierValidatorTest.java
@@ -7,35 +7,34 @@ import static org.hamcrest.Matchers.empty;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import com.exasol.errorcodecrawlermavenplugin.Finding;
 import com.exsol.errorcodemodel.ErrorMessageDeclaration;
 
 //[utest->dsn~identifier-validator~1]
 class ErrorIdentifierValidatorTest {
-    @Test
-    void testValid() {
-        final ErrorMessageDeclaration declaration = ErrorMessageDeclaration.builder().identifier("E-TEST-1").build();
+
+    @ParameterizedTest
+    @ValueSource(strings = { "E-TEST-1", "E-ZERO-0", "E-TEST1-2", "E-TEST-MOD-1", "E-lower-case-2", "W-WARN-3",
+            "F-FAIL-4", "E-VERYVERYLONGTAG-5", "E-TOO-MANY-MODULES-6" })
+    void testValidErrorIdentifiers(final String identifier) {
+        final ErrorMessageDeclaration declaration = ErrorMessageDeclaration.builder().identifier(identifier).build();
         assertThat(new ErrorIdentifierValidator().validate(List.of(declaration)), empty());
     }
 
-    @Test
-    void testInvalidType() {
-        final ErrorMessageDeclaration declaration = ErrorMessageDeclaration.builder().identifier("Q-TEST-1")
+    @ParameterizedTest
+    @CsvSource(quoteCharacter = '`', value = {
+            "Q-TEST-1, `E-ECMOJ-2: Illegal error code 'Q-TEST-1'. The codes must start with 'W-', 'E-' or 'F-'.`",
+            "123, `E-ECMOJ-1: The error code '123' has an invalid format.`" //
+    })
+    void testInvalidErrorIdentifier(final String identifier, final String expectedErrorMessage) {
+        final ErrorMessageDeclaration declaration = ErrorMessageDeclaration.builder().identifier(identifier)
                 .setPosition("Test.java", 1).build();
         final List<Finding> findings = new ErrorIdentifierValidator().validate(List.of(declaration));
         final List<String> findingMessages = findings.stream().map(Finding::getMessage).collect(Collectors.toList());
-        assertThat(findingMessages, contains(
-                "E-ECMOJ-2: Illegal error code 'Q-TEST-1'. The codes must start with 'W-', 'E-' or 'F-'. (Test.java:1)"));
-    }
-
-    @Test
-    void testInvalidFormat() {
-        final ErrorMessageDeclaration declaration = ErrorMessageDeclaration.builder().identifier("123")
-                .setPosition("Test.java", 1).build();
-        final List<Finding> findings = new ErrorIdentifierValidator().validate(List.of(declaration));
-        final List<String> findingMessages = findings.stream().map(Finding::getMessage).collect(Collectors.toList());
-        assertThat(findingMessages, contains("E-ECMOJ-1: The error code '123' has an invalid format. (Test.java:1)"));
+        assertThat(findingMessages, contains(expectedErrorMessage + " (Test.java:1)"));
     }
 }
