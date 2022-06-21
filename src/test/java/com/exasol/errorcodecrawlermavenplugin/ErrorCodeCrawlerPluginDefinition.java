ec309330d842fa620bb404e95b9e42778f456903
Christoph Pirkl <christoph.pirkl@exasol.com>
2022-06-21 18:32:17 +0200

    #73: Add test for skipping execution with "skip" configuration.

--- src/test/java/com/exasol/errorcodecrawlermavenplugin/ErrorCodeCrawlerPluginDefinition.java
+++ src/test/java/com/exasol/errorcodecrawlermavenplugin/ErrorCodeCrawlerPluginDefinition.java
@@ -5,10 +5,16 @@ import java.util.List;
 class ErrorCodeCrawlerPluginDefinition {
     private final String version;
     private final List<String> sourcePaths;
-
+    private final String skip;
+    
     public ErrorCodeCrawlerPluginDefinition(final String version, final List<String> sourcePaths) {
+        this(version, sourcePaths, null);
+    }
+
+    public ErrorCodeCrawlerPluginDefinition(final String version, final List<String> sourcePaths, final String skip) {
         this.version = version;
         this.sourcePaths = sourcePaths;
+        this.skip = skip;
     }
 
     public String getVersion() {
@@ -17,5 +23,9 @@ class ErrorCodeCrawlerPluginDefinition {
 
     public List<String> getSourcePaths() {
         return this.sourcePaths;
+    }
+
+    public String getSkip() {
+        return skip;
     }
 }
