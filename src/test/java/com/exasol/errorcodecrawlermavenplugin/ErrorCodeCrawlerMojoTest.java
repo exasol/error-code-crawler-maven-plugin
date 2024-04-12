2fb8997ade5af977fdff86b96aa7a2f58b1600b5
mykhailo.skliar <mykhailo.skliar@exasol.com>
2024-04-12 11:17:32 +0200

    #109: Fix The Source Path for Single-Module and Multi-Module Projects - added unit test, which reproduces the bug: the single-module project with parent pom should have correct source path (without project name as a prefix)

--- src/test/java/com/exasol/errorcodecrawlermavenplugin/ErrorCodeCrawlerMojoTest.java
+++ src/test/java/com/exasol/errorcodecrawlermavenplugin/ErrorCodeCrawlerMojoTest.java
@@ -84,6 +84,55 @@ class ErrorCodeCrawlerMojoTest {
         );
     }
 
+    @Test
+    void testProjectWithParentPomReport() throws IOException, MojoFailureException, ErrorCodeReportReader.ReadException {
+        Path projectPath = projectDir.toFile().getCanonicalFile().toPath();
+        final Path expectedPath = Path.of("src/main/java/com/exasol/errorcodecrawlermavenplugin/examples/Test1.java");
+
+        Path projectMainSrcJava = projectPath.resolve(Path.of("src", "main", "java"));
+        Path projectMainSrcPackage = projectMainSrcJava
+                .resolve(Path.of("com", "exasol", "errorcodecrawlermavenplugin", "examples"));
+        projectMainSrcPackage.toFile().mkdirs();
+        Files.copy(EXAMPLES_PATH.resolve("Test1.java"), //
+                projectMainSrcPackage.resolve("Test1.java"), //
+                StandardCopyOption.REPLACE_EXISTING);
+
+        runProjectWithParentPomErrorCodeCrawlerMojo(projectPath);
+
+        final ErrorCodeReport result = new ErrorCodeReportReader()
+                .readReport(projectPath.resolve("target/error_code_report.json"));
+        final List<ErrorMessageDeclaration> errorCodes = result.getErrorMessageDeclarations();
+        final ErrorMessageDeclaration first = errorCodes.get(0);
+        assertAll(//
+                () -> assertThat(errorCodes.size(), equalTo(1)),
+                () -> assertThat(first.getIdentifier(), equalTo("E-TEST-1")),
+                () -> assertThat(first.getSourceFile(), equalTo(expectedPath.toString())),
+                () -> assertThat(first.getLine(), equalTo(10)), //
+                () -> assertThat(first.getMessage(), equalTo("Test message"))//
+        );
+    }
+
+    private void runProjectWithParentPomErrorCodeCrawlerMojo(Path projectPath) throws MojoFailureException, IOException {
+        final MavenProject parentProject = new MavenProject();
+        Path parentProjectPath = this.projectDir.resolve("parent-pom");
+        parentProject.setFile(parentProjectPath.resolve("pom.xml").toFile());
+
+        final ErrorCodeCrawlerMojo errorCodeCrawlerMojo = new ErrorCodeCrawlerMojo();
+        final MavenProject project = new MavenProject();
+        project.setFile(projectPath.resolve("pom.xml").toFile());
+        project.setParent(parentProject);
+        errorCodeCrawlerMojo.project = project;
+        errorCodeCrawlerMojo.skip = "false";
+        errorCodeCrawlerMojo.executionRootDirectory = projectPath.toString();
+
+        final InputStream configStream = ErrorCodeCrawlerMojoIT.class.getClassLoader().getResourceAsStream("testProject/" + CONFIG_NAME);
+        Files.copy(Objects.requireNonNull(configStream), //
+                this.projectDir.resolve(CONFIG_NAME), //
+                StandardCopyOption.REPLACE_EXISTING);
+
+        errorCodeCrawlerMojo.execute();
+    }
+
     private void runSubProjectErrorCodeCrawlerMojo(Path projectPath, Path subProjectPath) throws MojoFailureException, IOException {
         final MavenProject project = new MavenProject();
         project.setFile(this.projectDir.resolve("pom.xml").toFile());
